7f435522e9d40f1d035342facd428838f6e57ec8
Francesco Paciello <kekkopacio@gmail.com>
2021-03-16 14:34:10 +0100

    Initial Commit

---
+++ FoodDelivery/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodDelivery
+//
+//  Created by Kekko Paciello on 16/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FoodDelivery/FoodDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodDeliveryApp.swift
+//  FoodDelivery
+//
+//  Created by Kekko Paciello on 16/03/21.
+//
+
+import SwiftUI
+
+@main
+struct FoodDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
